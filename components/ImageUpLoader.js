3415995e23d23b3bfdda7ce0527024aac8673590
Jemilu <mohammedjemilu@yahoo.com>
2019-09-24 21:32:42 +0000

    image-uploader styling complete

--- components/ImageUpLoader.js
+++ components/ImageUpLoader.js
@@ -7,34 +7,30 @@ import '../static/styles.css';
 
 const ImageUpLoader = (props) => {
     return (
-        <div>
-            <div id="main">
+        
+            <div id="image-uploader">
                 <p className="text-center">{props.header}</p>
-                <div className="p-2" id="upload-logo">
+                <div id="upload-box">
                         <label htmlFor="fileInput">
-                            <svg className="h-full w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm-6 336H54a6 6 0 0 1-6-6V118a6 6 0 0 1 6-6h404a6 6 0 0 1 6 6v276a6 6 0 0 1-6 6zM128 152c-22.091 0-40 17.909-40 40s17.909 40 40 40 40-17.909 40-40-17.909-40-40-40zM96 352h320v-80l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L192 304l-39.515-39.515c-4.686-4.686-12.284-4.686-16.971 0L96 304v48z"/>
-                            </svg>
+                        <svg className="h-full w-full" fill="grey" xmlns="http://www.w3.org/2000/svg" width="20" height="12" viewBox="0 0 24 24">
+                            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/>
+                        </svg>
                         </label>
-                        <input type="file" {...props} className="block" id="fileInput"/>
+                        <input type="file" {...props} className="hidden" id="fileInput"/>
                 </div>
                 <p className="text-xs">Maxi size of 1MB.JPG or PNG</p>
-            </div>
-        <style jsx>{`
-            #upload-logo {
-                border : 2px dashed grey;
-            }
 
-            #main {
-                max-width : 7rem;
-                min-height : 10rem;
-            }
-            #fileInput {
-                display:none;
-            }
-        `}
-        </style>
-        </div>
+                <style jsx>{`
+                    #upload-box {
+                        border : 2px dashed grey;
+                        max-width:9rem;
+                        max-height:7rem;
+                    }
+                `}
+                </style>
+            </div>
+        
+        
     );
 }
 
