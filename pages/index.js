86b0eea8974cf9305d4b6dbb776cf2387dd24b32
Muhammad <maej_01@hotmail.com>
2019-10-01 22:09:41 +0000

    fix: merge conflict

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 
 import { fade, makeStyles } from '@material-ui/core/styles';
 import Header from '../components/Header'
+import Footer from '../components/Footer';
 import bgImage from '../images/homeBg.svg'
 import {CreateCard, LoginTrigger} from '../components/createCard'
 import JobCards from '../components/jobCards';
@@ -22,23 +23,26 @@ const useStyles = makeStyles(theme => ({
 
 export default function Home(){
     const classes = useStyles()
-    return(
-        <div style={{display:'flex', flexDirection:'column'}}>
-            <div style={{position:'relative'}}>
-            <Header activePage={'home'}/>
-            </div>
-            <div  className={classes.mainBody}>
-                {/* Side filter will come right here on the left */}
-                <div>
-
+    
+    // render = () => {
+        return(
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <div style={{position:'relative'}}>
+                <Header activePage={'home'}/>
                 </div>
-                {/* Hooking the job cards right here */}
-                <div style={{paddingLeft:'400px', position:'relative',  justifyContent:'flex-start', alignSelf:'flex-start', marginLeft:'auto', marginRight:'4px'}}>
-                    <JobCards />
+                <div  className={classes.mainBody}>
+                    {/* Side filter will come right here on the left */}
+                    <div>
+    
+                    </div>
+                    {/* Hooking the job cards right here */}
+                    <div style={{paddingLeft:'400px', position:'relative',  justifyContent:'flex-start', alignSelf:'flex-start', marginLeft:'auto', marginRight:'4px'}}>
+                        <JobCards />
+                    </div>
                 </div>
-            </div>
-                    
-           
-        </div> 
-    );
+                <Footer />      
+            </div> 
+        );
+    // }
+    
 }
