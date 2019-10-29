7a95ad1abb24426ed5595dd57f595467f2ef405a
Muhammad Sesay <maej_01@hotmail.com>
2019-10-29 15:31:00 -0700

    update: server ip

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 // 18.197.130.180 || localhost:5000
-export const server = dev ? 'http://18.197.130.180/api/v1/portal' : 'https://your_deployment.server.com';
+export const server = dev ? 'localhost:5000/api/v1/portal' : 'https://18.197.130.180:50/api/v1/portal';
