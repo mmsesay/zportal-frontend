b3e2c6c71b29c71616ec4751438570c79f3eef98
Muhammad Sesay <maej_01@hotmail.com>
2019-10-29 18:19:58 -0700

    fix: create bg url

--- components/styles.js
+++ components/styles.js
@@ -1,8 +1,6 @@
   
 import { makeStyles } from '@material-ui/core/styles';
 import bgImage from '../images/NavBarBg.svg'
-import createBG from '../images/createBg.jpg'
-import loginBG from '../images/loginBG.jpg'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -143,23 +141,6 @@ const useStyles = makeStyles(theme => ({
       backgroundColor: '#1e5b49',
       fontWeight: 'bold',
       height:'100%',
-    },
-    createSection: {
-      backgroundImage: `url(${createBG})`,
-      backgroundRepeat:'no-repeat', 
-      backgroundSize:'cover', 
-      backgroundPosition: 'center',
-      width: 'auto',
-      height: '800px',
-    },
-    loginSection: {
-      backgroundImage: `url(${loginBG})`,
-      backgroundRepeat:'no-repeat', 
-      backgroundSize:'cover', 
-      backgroundPosition: 'center',
-      width: 'auto',
-      height: '800px',
-      margin: 0
     },
     loginForm: {
       marginLeft: '150px'
